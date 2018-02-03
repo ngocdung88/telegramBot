5beb24ea3fc579602fae70819008963fdfe430e8
duk <09520040@sv.uit.edu.vn>
2018-02-03 17:14:55 +0700

    optimize code

--- botUtil.js
+++ botUtil.js
@@ -3,7 +3,9 @@ const config = require('./config');
 const bot = botgram(config.token);
 const moment = require('moment');
 const cryptoService = require('./utils/cryptoService');
+const _ = require('lodash');
 let tele = require('./utils/telegram');
+let alert = [];
 
 bot.command("start", "help", (msg, reply) =>
     reply.text("To get total market cap, do: /mkcap"));
@@ -20,13 +22,59 @@ bot.command("mkcap", (msg, reply , next) => {
     });
 });
 
-bot.command('alert', (msg , reply) =>{
-    let chat_id = msg.user.id;
-    // console.log(msg);
-    tele.sendMessage(chat_id,'<b>heheh</b>','HTML', function (err, res) {
-        console.error(err);
-        console.log(res);
-    })
+bot.command('ping', (msg , reply, next) =>{
+    try {
+        let chat_id = msg.user.id;
+        var [seconds] = msg.args(1);
+        let schedule = {
+            chat_id : chat_id
+        };
+        let isExist = _.findIndex(alert, {chat_id : chat_id});
+
+        if (isExist > -1){
+            reply.text("currently,have a ping is set!");
+            return;
+        }
+
+        reply.text("Notify Total market Cap every " + (parseInt(seconds) || 60) + ' seconds');
+
+
+        let alertId  = setInterval(()=>{
+            cryptoService.getTotalMarket((err, data)=>{
+                if (err){
+                    next();
+                }else {
+                    tele.sendMessage(chat_id,'*Total Market Cap* : ' + data.total,'Markdown', function (err, res) {
+                        console.error(err);
+                        console.log(res);
+                    })
+                }
+            });
+        },parseInt(seconds)*1000 || 60000);
+        schedule.alertId = alertId;
+        alert.push(schedule);
+    } catch (e){
+        reply.text("Error command.");
+    }
+});
+
+
+bot.command('unping', (msg , reply, next) =>{
+    try {
+        let chat_id = msg.user.id;
+
+        let pingObj = _.remove(alert, {chat_id : chat_id});
+
+        if (_.first(pingObj)){
+            clearInterval(_.first(pingObj).alertId);
+        }
+
+
+        reply.text("Unping success");
+
+    } catch (e){
+        reply.text("Error command.");
+    }
 });
 
 bot.command((msg, reply) =>
